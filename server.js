99f37a916928840ccfe879bdbed42df203ece682
Tanijl Tuly <tanjiltuly@gmail.com>
2021-12-01 14:34:11 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+require('./models/db');
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+// const bodyparser = require('body-parser');
+// extname:".hbs", defaultLayout:'main'
+
+const employeeController = require('./controllers/employeeController');
+
+var app = express();
+// app.use(bodyparser.urlencoded({
+//     extended: true
+// }));
+app.use(express.json());
+const hbs = exphbs.create({ extname:".hbs" })
+
+
+// app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+app.listen(3000, () => {
+    console.log('Express server started at port : 3000');
+});
+
+app.use('/employee', employeeController);
